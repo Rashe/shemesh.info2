30c5973ae620c1b3ece98f74c3356bf4e8c3d28d
raphaelsh <shemeshra@gmail.com>
2015-05-27 17:29:28 +0300

    some fixes

--- controller/c-blog.js
+++ controller/c-blog.js
@@ -4,9 +4,9 @@ var data_content = require('../data/content');
 exports.get = function (req, res) {
     Blog.showAllPosts(function (posts) {
         if (posts.length) {
-            res.render('page-blog', {data: data_content, css: 'blog', posts: posts});
+            res.render('pages/blog/page-blog', {data: data_content, css: 'blog', posts: posts});
         } else {
-            res.render('page-blog', {data: data_content, css: 'blog'});
+            res.render('pages/blog/page-blog', {data: data_content, css: 'blog'});
         }
     });
 };
